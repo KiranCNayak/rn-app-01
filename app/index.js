203716de278b64c1e43e40bf9c0d0d0096dc5cf6
Kiran C Nayak <33034799+KiranCNayak@users.noreply.github.com>
2023-08-02 15:29:21 +0530

    Added assets and created dummy Home & Details page

--- app/index.js
+++ app/index.js
@@ -1,34 +1,30 @@
-import { StyleSheet, Text, View } from "react-native"
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import { useFonts } from "expo-font"
 
-export default function Page() {
+import Home from "../components/Home"
+import Details from "../components/Details"
+
+const Stack = createStackNavigator()
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+  },
+}
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
+    <NavigationContainer theme={theme} independent>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Home'>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Details' component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-})
+export default App
+
