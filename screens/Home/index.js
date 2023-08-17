06401d0486ea13567ae793e83a954eea01c25b41
Kiran C Nayak <33034799+KiranCNayak@users.noreply.github.com>
2023-08-17 11:45:10 +0530

    Added search functionality

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,5 +1,5 @@
 import { FlatList, SafeAreaView, View } from "react-native"
-import React from "react"
+import React, { useState } from "react"
 
 import FocusedStatusBar from "../../components/FocusedStatusBar"
 // eslint-disable-next-line import/no-named-as-default
@@ -9,19 +9,39 @@ import NFTCard from "../../components/NFTCard"
 import { COLORS, NFTData } from "../../constants"
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData)
+
+  const handleSearch = (value) => {
+    if (!value.length) return setNftData(NFTData)
+
+    const filteredData = NFTData.filter((item) => {
+      return item.name.toLowerCase().includes(value.toLowerCase())
+    })
+
+    if (filteredData.length) {
+      setNftData(filteredData)
+    } else {
+      setNftData(NFTData)
+    }
+  }
+
   return (
     // Adding a custom padding on top to have SafeAreaView work on Android
-    <SafeAreaView style={{ flex: 1,
-      paddingTop: 36 }}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        paddingTop: 36,
+      }}
+    >
       <FocusedStatusBar backgroundColor={COLORS.primary} />
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData}
+            data={nftData}
             renderItem={({ item }) => <NFTCard data={item} />}
-            keyExtractor={item => item.id}
+            keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader />}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
           />
           <View
             style={{
@@ -31,11 +51,20 @@ const Home = () => {
               right: 0,
               left: 0,
               zIndex: -1,
-            }}>
-            <View style={{ height: 300,
-              backgroundColor: COLORS.primary }} />
-            <View style={{ flex: 1,
-              backgroundColor: COLORS.white }} />
+            }}
+          >
+            <View
+              style={{
+                height: 300,
+                backgroundColor: COLORS.primary,
+              }}
+            />
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: COLORS.white,
+              }}
+            />
           </View>
         </View>
       </View>
